4b2bddff47a0d6a923a5382ecc09b7b253b703a4
Sonic-Shisui <botastro39@gmail.com>
2025-03-16 09:40:18 +0100

    Update say.js

--- scripts/cmds/say.js
+++ scripts/cmds/say.js
@@ -1,33 +1,50 @@
-const axios = require("axios");
+const axios = require('axios');
 
 module.exports = {
-  config: {
-    name: "say",
-    version: "1.1",
-    author: "Otinxsandip",
-    countDown: 5,
-    role: 0,
-    longDescription: "voice",
-    category: "ai",
-    guide: {
-      en: "{pn} text or reply to text"
-    }
-  },
+	config: {
+		name: "say",
+		aliases: ["speak"],
+		version: "1.0",
+		author: "Asmit Adk", // configured by ミ★𝐒𝐎𝐍𝐈𝐂✄𝐄𝐗𝐄 3.0★彡
+		countDown: 5,
+		role: 0,
+		shortDescription: "Say something Ai will say it clearly",
+		longDescription: "female  Voice in all languages",
+		category: "media",
+		guide: "{pn} {{<say>}}"
+	},
 
-  onStart: async function ({ api, event, args, getLang, message, usersData }) {
-    try {
-      const text = event.type === 'message_reply' ? event.messageReply.body : args.join(' ');
-      if (!text) {
-        return message.reply('please type text or reply to text');
+	onStart: async function ({ api, message, args, event}) {
+    let lng = "ne,fr,bn,en,in,hi,ar,as,bo,ja,kr,pa,sa,zh,vi,ur,uk,tr,th,ru"
+    let say;
+		if (lng.includes(args[0])) {
+      lng = args[0]
+      args.shift()
+      say = encodeURIComponent(args.join(" "))
+    } else { 
+      say = args.join(" ")
+    }
+	const extractText = () => {
+        if (type === "message_reply") {
+          return event.messageReply.body;
+        } else if (mentions.length > 0) {
+          return mentions[0].body;
+        } else {
+          return chat;
+        }
       }
-      const link = `https://sandipapi.onrender.com/say?text=${encodeURIComponent(text)}`;
+			try {
+				let url = `https://translate.google.com/translate_tts?ie=UTF-8&tl=${lng}&client=tw-ob&q=${say}`
 
-      message.reply({
-        body: 'here is your tts',
-        attachment: await global.utils.getStreamFromURL(link)
-      });
-    } catch (error) {
-      console.error(error);
-    }
-  }
+
+        message.reply({body:"",
+				attachment: await global.utils.getStreamFromURL(url)
+                      })
+				
+					
+			} catch (e) {
+        console.log(e)
+        message.reply(`🎙️🔊`) }
+
+	}
 };
