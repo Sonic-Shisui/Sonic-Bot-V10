9dde59ef51e136b7f43210afba28849e850d5952
Sonic-Shisui <botastro39@gmail.com>
2025-03-30 07:34:30 +0100

    Update wasted.js

--- scripts/cmds/wasted.js
+++ scripts/cmds/wasted.js
@@ -1,49 +1,56 @@
-const axios = require('axios');
-const fs = require('fs');
-module.exports = {
- config: {
+module.exports.config = {
   name: "wasted",
-  version: "1.1",
-  author: "OtinXSandip",
-  countDown: 5,
-  role: 0,
-  shortDescription: {
-    vi: "Làm thú cưng cho ai đó.",
-    en: "gay."
+  version: "1.0.1",
+  hasPermssion: 0,
+  credits: "Joshua Sy",
+  description: "communism",
+  commandCategory: "fun",
+  cooldowns: 2,
+  dependencies: {
+    canvas: "",
+    axios: "",
+    "fs-extra": "",
   },
-  longDescription: {
-    vi: "Làm thú cưng cho ai đó.",
-    en: "meme."
-  },
-  category: "fun",
-  guide: {
-    vi: "{pn} [ chỗ trống | trả lời | đề cập | uid ]",
-    en: "{pn} [ blank | reply | mention | uid ]"
-  }
- },
+};
 
- onStart: async function({ event, api, args , message }) {
-		const { threadID, messageID, senderID, body } = event;
-		let id;
-		if (args.join().indexOf('@') !== -1) {
-			id = Object.keys(event.mentions);
-		} else {
-			id = args[0] || senderID;
-		}
-		if (event.type == "message_reply") {
-			id = event.messageReply.senderID;
-		}
-		
-		const response = await axios.get(`https://sandip-api.onrender.com/wasted?uid=${id}`, { responseType: 'stream' });
-		const tempFilePath = './temp.png';
-		const writer = fs.createWriteStream(tempFilePath);
-		response.data.pipe(writer);
-		
-		writer.on('finish', async () => {
-			const attachment = fs.createReadStream(tempFilePath);
-			await api.sendMessage({ body: "", attachment: attachment }, threadID, messageID);
-			
-			fs.unlinkSync(tempFilePath);
-		});
-	}
+
+module.exports.run = async function ({ api, event, args, Users }) {
+  let { senderID, threadID, messageID } = event;
+  const { loadImage, createCanvas } = require("canvas");
+  const fs = global.nodemodule["fs-extra"];
+  const axios = global.nodemodule["axios"];
+  let pathImg = __dirname + "/cache/wanted.png";
+  let pathAva = __dirname + "/cache/avt.png";
+  if (!args[0]) { var uid = senderID}
+  if(event.type == "message_reply") { uid = event.messageReply.senderID }
+  if (args.join().indexOf('@') !== -1){ var uid = Object.keys(event.mentions) } 
+  let Avatar = (
+    await axios.get(
+      `https://graph.facebook.com/${uid}/picture?height=1500&width=1500&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`,
+      { responseType: "arraybuffer" }
+    )
+  ).data;
+  fs.writeFileSync(pathAva, Buffer.from(Avatar, "utf-8"));
+  let getWanted = (
+    await axios.get(`https://zenzapis.xyz/photoeditor/wasted?url=https://avatars.githubusercontent.com/u/68224412?v=4&apikey=7990c7f07144`, {
+      responseType: "arraybuffer",
+    })
+  ).data;
+  fs.writeFileSync(pathImg, Buffer.from(getWanted, "utf-8"));
+  let baseImage = await loadImage(pathImg);
+  let baseAva = await loadImage(pathAva);
+  let canvas = createCanvas(baseImage.width, baseImage.height);
+  let ctx = canvas.getContext("2d");
+  ctx.drawImage(baseAva, 0, 0, canvas.width, canvas.height);
+  ctx.drawImage(baseImage, 0, 0, canvas.width, canvas.height);
+  ctx.beginPath();
+  const imageBuffer = canvas.toBuffer();
+  fs.writeFileSync(pathImg, imageBuffer);
+  fs.removeSync(pathAva);
+  return api.sendMessage(
+    { attachment: fs.createReadStream(pathImg) },
+    threadID,
+    () => fs.unlinkSync(pathImg),
+    messageID
+  );
 };
