d6802c54de5c8bd31ed20f735313295c8b8b2069
Daniel <botastro39@gmail.com>
2025-04-26 21:35:51 +0100

    Create batslap.js

---
+++ scripts/cmds/batslap.js
@@ -0,0 +1,44 @@
+const DIG = require("discord-image-generation");
+const fs = require("fs-extra");
+
+module.exports = {
+  config: {
+    name: "batslap",
+    version: "1.1",
+    author: "ミ★𝐒𝐎𝐍𝐈𝐂✄𝐄𝐗𝐄 3.0★彡",
+    countDown: 5,
+    role: 0,
+    shortDescription: "Batslap image",
+    longDescription: "Batslap image",
+    category: "meme",
+    guide: {
+      en: "   {pn} @tag"
+    }
+  },
+
+  langs: {
+    vi: {
+      noTag: ""
+    },
+    en: {
+      noTag: "You must tag the person you want to slap"
+    }
+  },
+
+  onStart: async function ({ event, message, usersData, args, getLang }) {
+    const uid1 = event.senderID;
+    const uid2 = Object.keys(event.mentions)[0];
+    if (!uid2)
+      return message.reply(getLang("noTag"));
+    const avatarURL1 = await usersData.getAvatarUrl(uid1);
+    const avatarURL2 = await usersData.getAvatarUrl(uid2);
+    const img = await new DIG.Slap().getImage(avatarURL1, avatarURL2);
+    const pathSave = `${__dirname}/tmp/${uid1}_${uid2}slap.png`;
+    fs.writeFileSync(pathSave, Buffer.from(img));
+    const content = args.join(' ').replace(Object.keys(event.mentions)[0], "");
+    message.reply({
+      body: `${(content || "hehe boii")}`,
+      attachment: fs.createReadStream(pathSave)
+    }, () => fs.unlinkSync(pathSave));
+  }
+};
