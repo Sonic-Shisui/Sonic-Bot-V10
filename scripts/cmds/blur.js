345afeb3ca7917e7ca1e9d5698349f9150e00035
Daniel <botastro39@gmail.com>
2025-05-31 17:55:31 +0100

    Create blur.js

---
+++ scripts/cmds/blur.js
@@ -0,0 +1,62 @@
+const DIG = require("discord-image-generation");
+const fs = require("fs-extra");
+
+module.exports = {
+  config: {
+    name: "blur",
+    version: "1.0",
+    author: "ミ★𝐒𝐎𝐍𝐈𝐂✄𝐄𝐗𝐄 3.0★彡",
+    countDown: 1,
+    role: 0,
+    shortDescription: "blur User",
+    longDescription: "",
+    category: "fun",
+    guide: "{pn} {{[on | off]}}",
+    envConfig: {
+      deltaNext: 5
+    }
+  },
+
+  langs: {
+    vi: {
+      noTag: "User"
+    },
+    en: {
+      noTag: "tag"
+    }
+  },
+
+  onStart: async function ({ event, message, usersData, args, getLang }) 
+  {
+
+    let mention = Object.keys(event.mentions)
+    let uid;
+
+  
+
+    if(event.type == "message_reply"){
+    uid = event.messageReply.senderID
+    } else{
+      if (mention[0]){
+        uid = mention[0]
+      }else{
+        console.log(" jsjsj")
+        uid = event.senderID}
+    }
+
+let url = await usersData.getAvatarUrl(uid)
+let avt = await new DIG.Blur().getImage(url)
+
+
+ 
+      const pathSave = `${__dirname}/tmp/blur.png`;
+  fs.writeFileSync(pathSave, Buffer.from(avt));
+    let body = ""
+    if(!mention[0]) body=""
+    message.reply({body:body,
+attachment: fs.createReadStream(pathSave)
+    }, () => fs.unlinkSync(pathSave));
+
+
+  }
+};
